061538f4850ac99507aa979b9ea66605ed65f3fd
eyang12 <emily.yang@ipaper.com>
2023-07-07 12:44:50 -0400

    added some code

---
+++ testgui.py
@@ -0,0 +1,8 @@
+from sklearn import metrics
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Introduction to building Streamlit WebApp')
+st.sidebar.title('This is the sidebar')
+st.sidebar.markdown("Let's start with binary classification!!")
