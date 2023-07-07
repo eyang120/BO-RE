ca3739e6a2a7e37744044fd4d935a089898ef164
eyang12 <emily.yang@ipaper.com>
2023-07-07 13:44:52 -0400

    added other models

--- testgui.py
+++ testgui.py
@@ -2,8 +2,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import LinearRegression 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import Lasso, LinearRegression, Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split, cross_validate, cross_val_score
 import matplotlib.pyplot as plt
@@ -61,6 +62,7 @@ df = load()
 x = df.drop("RE test", axis = 1)
 y = df["RE test"]
 
+
 if st.sidebar.checkbox("Display data", False):
     st.subheader("Bogalusa Reduction Efficiency Dataset")
     st.write(df)
@@ -74,15 +76,19 @@ def feature_selection(data, x, y):
     return selected_df
 
 selected_df = feature_selection(df, x, y)
+x_train, x_test, y_train, y_test = train_test_split(selected_df, y, test_size=0.3)
+
 
 if st.sidebar.checkbox("Display LASSO-selected features", False):
     st.subheader("Selected BO-RE Features:")
     st.write(selected_df)
 
-@st.cache_data(persist=True)
-def model_building(x2, y):
-    x_train, x_test, y_train, y_test = train_test_split(x2, y, test_size=0.3)
+st.sidebar.subheader("Choose model:")
+model_select = st.sidebar.selectbox("Model", ("Linear", "Random Forest", "SVR", "Ridge", "Neutral Net", "XGBoost"))
 
+@st.cache_data(persist=True)
+def linear_model(x_train, x_test, y_train, y_test):
+    
     lr = LinearRegression()
 
     cv_rmse_scores = -cross_val_score(lr, x_train, y_train, scoring='neg_mean_squared_error', cv=5)
@@ -112,5 +118,107 @@ def model_building(x2, y):
     st.write("\nTest RMSE: ", test_rmse)
     st.write("Test R^2: ", test_r2)
 
+def random_forest(x_train, x_test, y_train, y_test):
+    rf_regressor = RandomForestRegressor(n_estimators=350)
+
+    cv_mse_scores = -cross_val_score(rf_regressor, x_train, y_train, scoring='neg_mean_squared_error', cv=5)
+    cv_r2_scores = 1 - (cv_mse_scores / np.var(y_train))
+
+    rf_regressor.fit(x_train, y_train)
+    y_pred = rf_regressor.predict(x_test)
+    test_rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+
+    test_r2 = r2_score(y_test, y_pred)
+
+    fig = plt.figure()
+    plt.scatter(y_pred, y_test, color='blue', label="Actual")
+    plt.title("Random Forest Regression - Actual vs Predicted")
+    plt.xlabel("Predicted values")
+    plt.ylabel("Actual values")
+    plt.legend()
+    st.pyplot(fig)
+
+    st.write("Cross-Validation RMSE Scores:")
+    st.write(np.sqrt(cv_mse_scores))
+    st.write("Average RMSE: ", np.mean(np.sqrt(cv_mse_scores)))
+    st.write("\nCross-Validation R^2 Scores:")
+    st.write(cv_r2_scores)
+    st.write("Average R^2: ", np.mean(cv_r2_scores))
+    st.write("\nTest RMSE: ", test_rmse)
+    st.write("Test R^2: ", test_r2)
+
+def svr_model(x_train, x_test, y_train, y_test):
+    svr = SVR(kernel='rbf')
+
+    cv_mse_scores = -cross_val_score(svr, x_train, y_train, scoring='neg_mean_squared_error', cv=5)
+    cv_r2_scores = cross_val_score(svr, x_train, y_train, scoring='r2', cv=5)
+
+    svr.fit(x_train, y_train)
+    y_pred = svr.predict(x_test)
+    test_rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+
+    test_r2 = r2_score(y_test, y_pred)
+
+    fig = plt.figure()
+    plt.scatter(y_pred, y_test, color='blue', label="Actual")
+    plt.title("SVM Regression - Actual vs Predicted")
+    plt.xlabel("Predicted values")
+    plt.ylabel("Actual values")
+    plt.legend()
+    st.pyplot(fig)
+
+    st.write("Cross-Validation RMSE Scores:")
+    st.write(np.sqrt(cv_mse_scores))
+    st.write("Average RMSE: ", np.mean(np.sqrt(cv_mse_scores)))
+    st.write("\nCross-Validation R^2 Scores:")
+    st.write(cv_r2_scores)
+    st.write("Average R^2: ", np.mean(cv_r2_scores))
+    st.write("\nTest RMSE: ", test_rmse)
+    st.write("Test R^2: ", test_r2)
+
+
+def ridge_model(x_train, x_test, y_train, y_test):
+    ridge = Ridge(alpha=1.0)
+
+    cv_mse_scores = -cross_val_score(ridge, x_train, y_train, scoring='neg_mean_squared_error', cv=5)
+    cv_r2_scores = cross_val_score(ridge, x_train, y_train, scoring='r2', cv=5)
+
+    ridge.fit(x_train, y_train)
+    y_pred = ridge.predict(x_test)
+
+    test_rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+
+    test_r2 = r2_score(y_test, y_pred)
+
+    fig = plt.figure()
+    plt.scatter(y_pred, y_test, color='blue', label="Actual")
+    plt.title("Ridge Regression - Actual vs Predicted")
+    plt.xlabel("Predicted values")
+    plt.ylabel("Actual values")
+    plt.legend()
+    st.pyplot(fig)
+
+    st.write("Cross-Validation RMSE Scores:")
+    st.write(np.sqrt(cv_mse_scores))
+    st.write("Average RMSE: ", np.mean(np.sqrt(cv_mse_scores)))
+    st.write("\nCross-Validation R^2 Scores:")
+    st.write(cv_r2_scores)
+    st.write("Average R^2: ", np.mean(cv_r2_scores))
+    st.write("\nTest RMSE: ", test_rmse)
+    st.write("Test R^2: ", test_r2)
+
+
 if st.button("Run Model"):
-    model_building(selected_df, y)
+    match model_select:
+        case "Linear":
+            linear_model(x_train, x_test, y_train, y_test)
+        case "Random Forest":
+            random_forest(x_train, x_test, y_train, y_test)
+        case "SVR":
+            svr_model(x_train, x_test, y_train, y_test)
+        case "Ridge":
+            ridge_model(x_train, x_test, y_train, y_test)
+        case "Neural Net":
+            pass
+        case "XGBoost":
+            pass
