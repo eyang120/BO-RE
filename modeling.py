0cfa22626635015b75216c5b311edb552343b733
mxia <maggie.xia@ipaper.com>
2023-07-10 12:12:12 -0400

    predict

--- modeling.py
+++ modeling.py
@@ -30,3 +30,39 @@ def correct_dtypes(df: pd.DataFrame):
         elif df[col].dtype not in numeric_types:
             df[col] = pd.to_numeric(df[col], errors="coerce")
     return df
+
+def correct_dtypes(df: pd.DataFrame):
+    """
+    Correct the data types of the BO RE data. Transform Timestamp into datetime format and force all numerical data.
+    """
+    if type(df) != pd.DataFrame:
+        raise TypeError("Data is not DataFrame!")
+    
+    numeric_types = (int, float)
+    
+    for col in df:
+        if col == "Timestamp":
+            df[col] = pd.to_datetime(df[col])
+        elif df[col].dtype not in numeric_types:
+            df[col] = pd.to_numeric(df[col], errors="coerce")
+    return df
+
+
+
+def gen_cleaning(df: pd.DataFrame):
+    """
+    General dataframe cleaning: 
+    - Drop rows were all values are NaN/NaT
+    - Drop columns with excessive NaN
+    - Interpolate missing values
+    - Remove outliers
+    - Set timestamp as index
+    """
+    df.dropna(inplace=True, how = 'all')
+    blank_cols = df.isnull().sum()[df.isnull().sum() > 10].index
+    #display(blank_cols)
+    df.drop(blank_cols, axis=1, inplace=True)
+    df = df.interpolate()
+    df = df[df['RE test'] >= 60]
+    df.set_index('Timestamp', inplace=True)
+    return df
--- testgui.py
+++ testgui.py
@@ -15,52 +15,16 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import xgboost as xgb
+import modeling 
 
 st.title('Bogalusa Reduction Efficiency Model')
 st.sidebar.title('Options')
 
-def correct_dtypes(df: pd.DataFrame):
-    """
-    Correct the data types of the BO RE data. Transform Timestamp into datetime format and force all numerical data.
-    """
-    if type(df) != pd.DataFrame:
-        raise TypeError("Data is not DataFrame!")
-    
-    numeric_types = (int, float)
-    
-    for col in df:
-        if col == "Timestamp":
-            df[col] = pd.to_datetime(df[col])
-        elif df[col].dtype not in numeric_types:
-            df[col] = pd.to_numeric(df[col], errors="coerce")
-    return df
-
-
-
-def gen_cleaning(df: pd.DataFrame):
-    """
-    General dataframe cleaning: 
-    - Drop rows were all values are NaN/NaT
-    - Drop columns with excessive NaN
-    - Interpolate missing values
-    - Remove outliers
-    - Set timestamp as index
-    """
-    df.dropna(inplace=True, how = 'all')
-    blank_cols = df.isnull().sum()[df.isnull().sum() > 10].index
-    #display(blank_cols)
-    df.drop(blank_cols, axis=1, inplace=True)
-    df = df.interpolate()
-    df = df[df['RE test'] >= 60]
-    df.set_index('Timestamp', inplace=True)
-    return df
-
-
 @st.cache_data(persist= True)
 def load():
     data = pd.read_csv("BORE_v3.csv")
-    data = correct_dtypes(data)
-    data = gen_cleaning(data)
+    data = modeling.correct_dtypes(data)
+    data = modeling.gen_cleaning(data)
     return data
 df = load()
 x = df.drop("RE test", axis = 1)
@@ -107,6 +71,14 @@ with st.sidebar:
 
 
 @st.cache_data(persist=True)
+
+
+def linear_predict(x_train, x_test, y_train):
+    lr = LinearRegression()
+    lr.fit(x_train, y_train)
+    y_pred = lr.predict(x_test)
+    return y_pred[-1]
+
 def linear_model(x_train, x_test, y_train, y_test):
     
     lr = LinearRegression()
@@ -387,4 +359,7 @@ if predict_button:
     retrieve_slider_values()
     new_row = pd.DataFrame(slider_values, index=[0])
     x_test2 = pd.concat([x_test, new_row], ignore_index=True)
-    
+    match model_select:
+        case "Linear":
+            y_pred = linear_predict(x_train, x_test2, y_train)
+            st.write(f"y_pred based off slider values: {y_pred}")
